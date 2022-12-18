8ad28c1b7ddb7ff484f35c4fa932a51e7c61c6b3
今井大貴 <i.hlrrr0@gmail.com>
2022-12-18 09:19:04 +0900

    wip

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require 'date'
 require 'pry'
 require 'pry-nav'
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+wait = Selenium::WebDriver::Wait.new(:timeout => 3) # seconds
 
 # USER_AGENT = "Mac Safari' => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; de-at) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10"
 setting = Selenium::WebDriver::Chrome::Options.new
@@ -18,13 +18,12 @@ driver = Selenium::WebDriver.for :chrome, options: setting
 
 
 # 2秒待っても読み込まれない場合は、エラーが発生する
-driver.manage.timeouts.implicit_wait = 1
+driver.manage.timeouts.implicit_wait = 0.3
 # ページ遷移する
 
 rows = []
 
 driver.navigate.to "https://shopping.yahoo.co.jp/search?X=2"
-sleep(2)
 # unless driver.find_element(:css, '.Sq9SEWjoMWXj').nil?
 #   driver.find_element(:css, '.Sq9SEWjoMWXj').click
 # end
@@ -32,33 +31,52 @@ sleep(2)
 
 File.open('import_data.txt') do |file|
   file.each_line do |jan_code|
-    jan_code = jan_code.gsub(/\R| / ,"")
+    p jan_code = jan_code.gsub(/\R| / ,"")
 
+    sleep(3) 
     driver.find_element(:name, 'p').clear
     query = driver.find_element(:name, 'p')
     query.send_keys(jan_code)
     query.submit
 
-    sleep(1)
     # driver.execute_script('window.scroll(0,1000);')
     item_data = []
 
-    item_cards = driver.find_elements(:css, '.LoopList__item')
-    item_cards.each do |item_card|
-      price = item_card.find_element(:css,'.L6licUwI07IZ').text
-      price = price.gsub(/\￥|\+送料|,|円/ ,"")
-      shipping_price = item_card.find_element(:css,'.sSvkZS-NYWEN').text
-      shipping_price = shipping_price.gsub(/\￥|\+送料|,|円/ ,"")
-      per_item_price = price.to_i + shipping_price.to_i
-      item_data << per_item_price
+    wait.until do
+      driver.find_elements(:css,'.sSvkZS-NYWEN')[0].displayed?
+
+      item_cards = driver.find_elements(:css, '.LoopList__item')
+      item_cards.each do |item_card|
+
+        price = 0
+        shipping_price = 0
+
+        price = item_card.find_element(:css,'.L6licUwI07IZ').text
+        p price = price.gsub(/\￥|\+送料|,|円/ ,"")
+
+        shipping_ele = item_card.find_elements(:css,'.sSvkZS-NYWEN')
+
+        unless shipping_ele.nil?
+          shipping_price = shipping_ele[0].text
+          p shipping_price = shipping_price.gsub(/\￥|\+送料|,|円/ ,"")
+        end
+
+        per_item_price = price.to_i + shipping_price.to_i
+        item_data << per_item_price
+        item_data.delete("")
+        item_data.delete(0)
+        item_data.delete("0")
+      rescue => e
+        p e.message
+      end
+      p "--"
+      p min_per_item_price = item_data.min
+
+      rows << [jan_code, min_per_item_price]
+
     rescue => e
       p e.message
-    end
-    p "--"
-    p min_per_item_price = item_data.min
-
-    rows << [jan_code, min_per_item_price]
-  
+    end  
   rescue => e
     p e.message
   end
